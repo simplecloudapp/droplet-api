28f45103cb6e854397bd2e71154b034d6ab63887
dayeeet <david@cappell.net>
2024-12-03 17:10:59 +0100

    feat: add release workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,16 @@
 plugins {
     alias(libs.plugins.kotlin)
-    alias(libs.plugins.shadow)
+    alias(libs.plugins.sonatype.central.portal.publisher)
     `maven-publish`
 }
 
+val baseVersion = "0.0.30"
+val commitHash = System.getenv("COMMIT_HASH")
+val snapshotversion = "${baseVersion}-dev.$commitHash"
+
 allprojects {
     group = "app.simplecloud.droplet.api"
-    version = "0.0.1-EXPERIMENTAL"
+    version = if (commitHash != null) snapshotversion else baseVersion
 
     repositories {
         mavenCentral()
@@ -14,37 +18,83 @@ allprojects {
     }
 }
 
-subprojects {
-    apply(plugin = "org.jetbrains.kotlin.jvm")
-    apply(plugin = "com.gradleup.shadow")
-    apply(plugin = "maven-publish")
+dependencies {
+    testImplementation(rootProject.libs.kotlin.test)
+    implementation(rootProject.libs.kotlin.jvm)
+}
 
-    dependencies {
-        testImplementation(rootProject.libs.kotlin.test)
-        implementation(rootProject.libs.kotlin.jvm)
-    }
-
-    publishing {
-        publications {
-            create<MavenPublication>("mavenJava") {
-                from(components["java"])
+publishing {
+    repositories {
+        maven {
+            name = "simplecloud"
+            url = uri("https://repo.simplecloud.app/snapshots/")
+            credentials {
+                username = System.getenv("SIMPLECLOUD_USERNAME")?: (project.findProperty("simplecloudUsername") as? String)
+                password = System.getenv("SIMPLECLOUD_PASSWORD")?: (project.findProperty("simplecloudPassword") as? String)
+            }
+            authentication {
+                create<BasicAuthentication>("basic")
             }
         }
     }
 
-    java {
-        toolchain.languageVersion.set(JavaLanguageVersion.of(22))
-    }
-
-    kotlin {
-        jvmToolchain(22)
-        compilerOptions {
-            apiVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0)
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_22)
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
         }
     }
+}
 
-    tasks.test {
-        useJUnitPlatform()
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(22))
+}
+
+kotlin {
+    jvmToolchain(22)
+    compilerOptions {
+        apiVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0)
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_22)
     }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+centralPortal {
+    name = project.name
+
+    username = project.findProperty("sonatypeUsername") as? String
+    password = project.findProperty("sonatypePassword") as? String
+
+    pom {
+        name.set("SimpleCloud Droplet API")
+        description.set("Commonly used classes among droplets/services")
+        url.set("https://github.com/theSimpleCloud/droplet-api")
+        developers {
+            developer {
+                id.set("dayyeeet")
+                email.set("david@cappell.net")
+            }
+        }
+        licenses {
+            license {
+                name.set("Apache-2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        scm {
+            url.set("https://github.com/theSimpleCloud/droplet-api.git")
+            connection.set("git:git@github.com:theSimpleCloud/droplet-api.git")
+        }
+    }
+}
+
+signing {
+    if (commitHash != null) {
+        return@signing
+    }
+
+    sign(publishing.publications)
+    useGpgCmd()
 }
