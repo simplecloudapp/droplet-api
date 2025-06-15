1a8cf6ee72bbfec43735cbba0c9cf0829d89dfc0
fllipeis <p.eistrach@gmail.com>
2025-06-15 16:18:12 +0200

    fix: identifier function

--- src/main/kotlin/app/simplecloud/droplet/api/droplet/DropletIdentifier.kt
+++ src/main/kotlin/app/simplecloud/droplet/api/droplet/DropletIdentifier.kt
@@ -2,19 +2,21 @@ package app.simplecloud.droplet.api.droplet
 
 import java.nio.file.Paths
 import java.util.UUID
+import kotlin.io.path.exists
 import kotlin.io.path.readText
 import kotlin.io.path.writeText
 
 object DropletIdentifier {
+
     fun getOrCreate(): String {
         val path = Paths.get(".sc.id")
-        var id = path.readText(Charsets.UTF_8)
-        try {
-            UUID.fromString(id)
-        }catch (e: Exception) {
-            id = UUID.randomUUID().toString()
+        if (!path.exists()) {
+            val id = UUID.randomUUID().toString()
             path.writeText(id, Charsets.UTF_8)
+            return id
         }
-        return id
+
+        return path.readText(Charsets.UTF_8)
     }
+
 }
